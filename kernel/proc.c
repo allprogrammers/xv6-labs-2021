13a66cbcf8690dbcd8d350076b6daf3befb09955
Muhammad Hamza Ali <hamzaali44@gmail.com>
2022-05-20 16:51:26 +0200

    implements trace system call

--- kernel/proc.c
+++ kernel/proc.c
@@ -164,6 +164,7 @@ freeproc(struct proc *p)
   p->killed = 0;
   p->xstate = 0;
   p->state = UNUSED;
+  p->trace_mask = 0;
 }
 
 // Create a user page table for a given process,
@@ -314,6 +315,8 @@ fork(void)
   acquire(&np->lock);
   np->state = RUNNABLE;
   release(&np->lock);
+
+  np->trace_mask = p->trace_mask;
 
   return pid;
 }
